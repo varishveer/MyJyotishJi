0f2a194e0db1827b8a6323bd8597d4fa266c4908
varishveer <variveersingh123@gmail.com>
2024-08-08 19:02:29 +0530

    .

--- ModelAccessLayer/Models/JyotishModel.cs
+++ ModelAccessLayer/Models/JyotishModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,25 @@ namespace ModelAccessLayer.Models
 {
     public class JyotishModel
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public DateOnly DateOfBirth { get; set; }
+        [Required]
         public string Gender { get; set; }
+        [Required]
         public string Language { get; set; }
+        [Required]
         public string Extertise { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Mobile { get; set; }
+        [Required]
         public string ProfileImageUrl { get; set; }
+        [Required]
         public string? Experience { get; set; }
         public string? Pooja { get; set; }
         public string? Password { get; set; }
