e16108c8110e1d1472aec8fa6589fa549e8f9573
varishveer <variveersingh123@gmail.com>
2024-08-31 18:59:14 +0530

    ..

---
+++ MyJyotishJiWebDesign/Controllers/ServicesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyJyotishGWeb.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Astrology()
+        {
+            return View();
+        }
+    }
+}
